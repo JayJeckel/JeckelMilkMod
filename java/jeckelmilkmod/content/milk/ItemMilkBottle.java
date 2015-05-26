61a0741ac08806ae22718e2fa94c0bfe7bd73451
JayJeckel <jeckelmail@gmail.com>
2015-05-26 00:28:34 -0500

    Add tooltip to milk bottle

--- java/jeckelmilkmod/content/milk/ItemMilkBottle.java
+++ java/jeckelmilkmod/content/milk/ItemMilkBottle.java
@@ -1,5 +1,7 @@
 package jeckelmilkmod.content.milk;
 
+import java.util.List;
+
 import jeckelmilkmod.core.Refs;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
@@ -8,6 +10,8 @@ import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemMilkBottle extends Item
 {
@@ -34,5 +38,12 @@ public class ItemMilkBottle extends Item
 	{
 		player.setItemInUse(stack, this.getMaxItemUseDuration(stack));
 		return stack;
+	}
+
+	@SuppressWarnings("unchecked")
+	@SideOnly(Side.CLIENT)
+	@Override public void addInformation(ItemStack stack, EntityPlayer player, @SuppressWarnings("rawtypes") List infoList, boolean par4)
+	{
+		infoList.add("Drink me to cure debuffs.");
 	}
 }
